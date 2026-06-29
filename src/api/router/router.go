823db8396df601df117845cf4da615c31273d9d9
Ayush <ayushtpr@gmail.com>
2026-06-29 20:23:34 +0530

    feat: implement the route for create update and get title of document

--- src/api/router/router.go
+++ src/api/router/router.go
@@ -2,9 +2,10 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ayush-0192/SyncPad/src/api/document"
 )
 
-func Setup() *gin.Engine {
+func Setup(docHandler *document.Handler) *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/health", func(c *gin.Context) {
@@ -12,6 +13,11 @@ func Setup() *gin.Engine {
 			"status": "ok",
 			"message": "SyncPad API is running",
 		})
-	})
+	}) 
+
+	r.POST("/create-document", docHandler.CreateDoc)
+	r.GET("/get-document/:id", docHandler.GetDocByid)
+	r.GET("/get-document-title-list", docHandler.GetAllDocTitleList)
+	r.PATCH("/update-doc",docHandler.GetAllDocTitleList)
 	return r
 }
