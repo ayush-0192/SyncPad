247ef9601514848d90e21306be81441bb9283844
Ayush <ayushtpr@gmail.com>
2026-06-30 15:43:55 +0530

    refactor: use documentsummary type to show the doucment title list

--- src/web/src/component/Sidebar.tsx
+++ src/web/src/component/Sidebar.tsx
@@ -4,25 +4,25 @@ import Link from "next/link"
 import { useDocument } from "@/hooks/useDocument"
 
 export default function Sidebar() {
-    const {documents, createDocument} = useDocument()
+    const {documents, createDocument,documentSummary} = useDocument()
 
     return (
         <aside className="w-72 border-r border-black flex flex-col">
             <button
                onClick={createDocument}
-                className="m-4 p-3 bg-black rounded-lg text-white"
+                className="m-4 p-3  rounded-lg text-white"
             >
                 + New Document
             </button>
 
             <div className="flex-1 overflow-auto">
-                {documents.map((doc) => (
+                {documentSummary.map((doc) => (
                     <Link
                     key={doc.id}
                     href={`/doc/${doc.id}`}
                     >
-                        <div className="p-3 hover:bg-gray-100">
-                            doc.title
+                        <div className="p-3 hover:bg-gray-100 text-white">
+                            {doc.title}
                         </div>
                     </Link>
                 ))}
