7e03cd4c762102625f6f32370c47fc76f4fe9885
Ayush <ayushtpr@gmail.com>
2026-06-30 15:37:44 +0530

    feat: add CORS configuration to the router for handling cross-origin requests

--- src/api/router/router.go
+++ src/api/router/router.go
@@ -3,10 +3,19 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ayush-0192/SyncPad/src/api/document"
+	"github.com/gin-contrib/cors"
 )
 
 func Setup(docHandler *document.Handler) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+    	AllowOrigins:     []string{"http://localhost:3000"},
+    	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+    	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+    	ExposeHeaders:    []string{"Content-Length"},
+    	AllowCredentials: true,
+	}))
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
