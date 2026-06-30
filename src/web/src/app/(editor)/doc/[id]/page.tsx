6c23144c08bf26bf4e273c717f70b2f5344161f9
Ayush <ayushtpr@gmail.com>
2026-06-30 15:45:03 +0530

    feat: implement the hook to fetch the full document when user click on sidebar document title

--- src/web/src/app/(editor)/doc/[id]/page.tsx
+++ src/web/src/app/(editor)/doc/[id]/page.tsx
@@ -6,6 +6,24 @@ import { useState } from "react";
 export default function DocumentPage() {
     const {id} = useParams()
     const[text, setText] = useState('')
+    useEffect(() => {
+        const fetchDocumentContent = async () => {
+            try {
+                    const response = await fetch(`http://localhost:8080/get-document/${id}`, {
+                        method: "GET",
+                    })
+                    if(!response.ok) {
+                    throw new Error("Failed to fetch document")
+                }
+                const data:FullDocument = await response.json();
+                setText(data.content)
+                lastSaved.current = data.content
+            }catch(error) {
+             console.log(error)
+            }
+        }
+        fetchDocumentContent()
+    },[id]);
     return (
         <div className="p-20">
             <Editor 
